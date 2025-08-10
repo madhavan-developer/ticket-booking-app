799b44c00fa1d280e47196822363fe083682b78a
madhavan-developer <madhavangopideveloper@gmail.com>
2025-08-11 00:35:24 +0530

    fixed code

--- server/server.js
+++ server/server.js
@@ -1,28 +1,28 @@
 import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
-import connectDB from './configs/db.js';
-import { clerkMiddleware } from '@clerk/express'
-import { serve } from "inngest/express";
-import { inngest, functions } from "./inngest/index.js"
+import connectDB from './configs/db.js'
+import { clerkMiddleware } from '@clerk/express';
+import { serve } from 'inngest/express';
+import { inngest, functions } from './inngest/index.js';
+import serverless from 'serverless-http';
 
 const app = express();
-const port = 3000;
 
 // Middleware
 app.use(express.json());
-app.use(cors()); 
-app.use(clerkMiddleware())
+app.use(cors());
+app.use(clerkMiddleware());
 
+// DB connection
+await connectDB();
 
-await connectDB()
-
-// API Routes
-app.get('/', (req, res) => {  res.send('Server is connected');});
-app.use("/api/inngest", serve({ client: inngest, functions }));
-
-
-// Start the server
-app.listen(port, () => {
-  console.log(`Server is running on port: ${port}`);
+// Routes
+app.get('/', (req, res) => {
+  res.send('Server is connected');
 });
+
+app.use('/api/inngest', serve({ client: inngest, functions }));
+
+// Export as serverless function for Vercel
+export default serverless(app);
