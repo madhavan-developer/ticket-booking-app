bedab20cd065141fe209e5885021f8470206d648
madhavan-developer <madhavangopideveloper@gmail.com>
2025-08-11 15:09:07 +0530

    change the structures

--- server/api/index.js
+++ server/api/index.js
@@ -1,10 +1,10 @@
 import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
-import connectDB from './configs/db.js'; // adjust path if needed
+import connectDB from '../configs/db.js';
 import { clerkMiddleware } from '@clerk/express';
 import { serve } from 'inngest/express';
-import { inngest, functions } from './inngest/index.js';
+import { inngest, functions } from '../inngest/index.js';
 import serverless from 'serverless-http';
 
 const app = express();
